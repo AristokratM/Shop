ba997f483523e844472d673e437bebc2f759256a
AristokratM <61250102+AristokratM@users.noreply.github.com>
2020-11-19 15:58:45 +0200

    List of products

--- shop/mainapp/admin.py
+++ shop/mainapp/admin.py
@@ -1,29 +1,23 @@
 from django.contrib import admin
 from django import forms
+from django.forms import  ModelChoiceField
 # Register your models here.
 from .models import *
-
-class NotebookCategoryChoiceField(forms.ModelChoiceField):
-    pass
 
 
 class NotebookAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug="notebooks"))
+            return ModelChoiceField(Category.objects.filter(slug="notebooks"))
         return super().formfield_for_foreignkey()
-
-
-class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
-    pass
 
 
 class SmartphoneAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NotebookCategoryChoiceField(Category.objects.filter(slug="smartphones"))
+            return ModelChoiceField(Category.objects.filter(slug="smartphones"))
         return super().formfield_for_foreignkey()
 
 
