dec57e9256763a9fcbf22efcf493167d4a58fad7
AristokratM <61250102+AristokratM@users.noreply.github.com>
2020-11-25 00:35:18 +0200

    Category endpoint

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -25,3 +25,12 @@ class ProductDetailView(DetailView):
     context_object_name = 'product'
     template_name = 'shop/product_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class CategoryDetailView(DetailView):
+
+    model = Category
+    queryset = Category.object.all()
+    context_object_name = 'category'
+    template_name = 'shop/category_detail.html'
+    slug_url_kwarg = 'slug'
