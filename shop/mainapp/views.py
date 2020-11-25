1b4fcad24b0b39925a964e2e51b5e1b738486265
AristokratM <61250102+AristokratM@users.noreply.github.com>
2020-11-25 12:14:39 +0200

    Add products to cart

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,14 +1,17 @@
+from django.contrib.contenttypes.models import ContentType
 from django.shortcuts import render
 from django.views.generic import DetailView, View
-from .models import Notebook, Smartphone, Category, LatestProducts, Cart, Customer
+from .models import Notebook, Smartphone, Category, LatestProducts, Cart, Customer, CartProduct
 from .mixins import CategoryDetailMixin
-
+from django.http import HttpResponseRedirect
 # Create your views here.
 
 
 class BaseView(View):
 
     def get(self, request, *args, **kwargs):
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
         categories = Category.object.get_categories_for_left_sidebar()
         products = LatestProducts.objects.get_products_for_main_page(
             'notebook', 'smartphone', with_respect_to='notebook'
@@ -16,9 +19,25 @@ class BaseView(View):
         print(products)
         context = {
             'categories': categories,
-            'products': products
+            'products': products,
+            'cart': cart,
         }
         return render(request, 'shop/base.html', context)
+
+
+class AddToCartView(View):
+
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product = CartProduct.objects.create(
+            user=cart.owner, cart=cart, content_type=content_type, object_id=product.id, final_price=product.price
+        )
+        cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
 
 
 class ProductDetailView(CategoryDetailMixin, DetailView):
@@ -35,6 +54,11 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     context_object_name = 'product'
     template_name = 'shop/product_detail.html'
     slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['ct_model'] = self.model._meta.model_name
+        return context
 
 
 class CategoryDetailView(CategoryDetailMixin, DetailView):
